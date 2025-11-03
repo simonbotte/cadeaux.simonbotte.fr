5f160d7aabe1a9e4dfa61730ba5669c3716cc708
simonbotte <simon.botte@icloud.com>
2025-11-03 19:03:48 +0100

    reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        // SCSS file in the project
-        "@/assets/css/main.scss",
-        "@/assets/css/button.scss",
-    ],
-    runtimeConfig: {
-        apiToken: process.env.NUXT_STRAPI_TOKEN,
-        public: {
-            apiUrl: "http://localhost:1337/api",
-        },
-    },
-    ssr: true,
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', '@nuxt/fonts']
+})
