63d91b726a2507c872d856528b6714df57affbca
simonbotte <simon.botte@icloud.com>
2025-11-10 18:20:14 +0100

    Use local image + reorganisation

--- app/app.vue
+++ app/app.vue
@@ -28,25 +28,25 @@ const categories = reactive([
             {
                 title: "Tananarive",
                 description: "Road-trip intime.",
-                image: "https://external-content.duckduckgo.com/iu/?u=https://static.fnac-static.com/multimedia/Images/FR/NR/da/05/cc/13370842/1507-1/tsp20210708130547/Tananarive.jpg&f=1",
+                image: "/img/gift/tananarive.jpg",
                 link: "https://www.fnac.com/a15812154/Mark-Eacersall-Tananarive",
             },
             {
                 title: "Jours de sable",
                 description: "Dust Bowl, photo.",
-                image: "https://static.fnac-static.com/multimedia/PE/Images/FR/NR/e3/37/cb/13318115/1507-1/tsp20250429080615/Jours-de-sable.jpg",
+                image: "/img/gift/jours-de-sable.jpg",
                 link: "https://www.fnac.com/a15760835/Aimee-de-Jongh-Jours-de-sable",
             },
             {
                 title: "Les Indes fourbes",
                 description: "Aventure picaresque.",
-                image: "https://static.fnac-static.com/multimedia/Images/FR/NR/a2/da/29/19520162/1507-1/tsp20251022081034/Les-indes-fourbes.jpg",
+                image: "/img/gift/les-indes-fourbes.jpg",
                 link: "https://www.fnac.com/a22208250/Juanjo-Guarnido-Les-indes-fourbes",
             },
             {
                 title: "La route",
                 description: "Père-fils, survie.",
-                image: "https://static.fnac-static.com/multimedia/PE/Images/FR/NR/c2/51/ff/16732610/1507-1/tsp20250429100750/La-route.jpg",
+                image: "/img/gift/la-route.jpg",
                 link: "https://www.fnac.com/a19356331/Manu-Larcenet-La-route",
             },
         ],
@@ -57,20 +57,26 @@ const categories = reactive([
             {
                 title: "Coffret The Dark Knight 4K",
                 description: "Le meilleurs de Batman.",
-                image: "https://static.fnac-static.com/multimedia/Images/FR/NR/5b/a1/e0/14721371/1540-1/tsp20221011170122/Coffret-The-Dark-Knight-Edition-Speciale-Fnac-Blu-ray-4K-Ultra-HD.jpg",
+                image: "/img/gift/bluray-batman.jpg",
                 link: "https://www.fnac.com/a17374716/Coffret-The-Dark-Knight-Edition-Speciale-Fnac-Blu-ray-4K-Ultra-HD-Christian-Bale-Blu-ray-4K",
             },
             {
                 title: "Coloretto",
                 description: "Jeu de combinaison et de prise de risque.",
-                image: "https://www.espritjeu.com/upload/image/coloretto-p-image-87155-grande.jpg",
+                image: "/img/gift/coloretto.webp",
                 link: "https://www.espritjeu.com/jeu-de-societe/coloretto.html?srsltid=AfmBOopuJjSSAv2k4fw2yluWBMUAloMqilbv38HzMb4V9RE9fNgIH-fV",
             },
             {
                 title: "7 Yokai",
                 description: "Jeu de plis asymétrique.",
-                image: "https://cdn2.philibertnet.com/726219-large_default/yokai-septet.jpg",
+                image: "/img/gift/jeu-yokai-septet.jpg",
                 link: "https://www.espritjeu.com/7-yokai.html?srsltid=AfmBOoqW4RvXwuoLzeFFPJeRqhc5SzYgMhmlczutRU1dVsWT3NhFG7qt",
+            },
+            {
+                title: "Lego La tête dans les étoiles avec Pharrell Williams",
+                description: "C'est magnifique non ?",
+                image: "/img/gift/lego-pharell.webp",
+                link: "https://www.fnac.com/LEGO-Icons-10391-La-tete-dans-les-etoiles-avec-Pharrell-Williams-Exclusivite-Fnac/a20974622/w-4?storecode=6902&Origin=SEA_GOOGLE_PLA_JOUET&esl-k=sem-google%7Cnx%7Cc%7Cm%7Ck%7Cp%7Ct%7Cdm%7Ca20461236398%7Cg20461236398&gclsrc=aw.ds&gad_source=1&gad_campaignid=20455746609&gclid=CjwKCAiAt8bIBhBpEiwAzH1w6fnK0SrFgRSVS74gShFIssUkK-NtBy-bFTrwATiAMHd0PT4Wc42CxxoCf2IQAvD_BwEhat/198077201458.html",
             },
         ],
     },
@@ -80,20 +86,37 @@ const categories = reactive([
             {
                 title: "Sauteuse Tefal",
                 description: "Anti-adhésive.",
-                image: "https://images-eu.ssl-images-amazon.com/images/I/71EIvV1dWqL._AC_UL348_SR348,348_.jpg",
+                image: "/img/gift/sauteuse.jpg",
                 link: "https://amzn.eu/d/csJ1Fwf",
             },
             {
                 title: "Poignée Tefal",
                 description: "Amovible, Ingenio.",
-                image: "https://images-eu.ssl-images-amazon.com/images/I/61nrhw0b+1L._AC_UL348_SR348,348_.jpg",
+                image: "/img/gift/poignee-tefal.jpg",
                 link: "https://amzn.eu/d/9vqMBPP",
             },
             {
                 title: "Air fryer Ninja",
                 description: "Sans huile.",
-                image: "https://m.media-amazon.com/images/I/71lLLGyzpBL._AC_SX679_.jpg",
+                image: "/img/gift/airfryer.jpg",
                 link: "https://amzn.eu/d/ceVs1Vb",
+            },
+        ],
+    },
+    {
+        name: "Vêtements",
+        items: [
+            {
+                title: "Sweat à capuche",
+                description: "S'habiller comme un développeur",
+                image: "/img/gift/sweat.jpg",
+                link: "https://https://www.uniqlo.com/fr/fr/products/E471808-000/00?colorDisplayCode=58&sizeDisplayCode=005storecode=6902&Origin=SEA_GOOGLE_PLA_JOUET&esl-k=sem-google%7Cnx%7Cc%7Cm%7Ck%7Cp%7Ct%7Cdm%7Ca20461236398%7Cg20461236398&gclsrc=aw.ds&gad_source=1&gad_campaignid=20455746609&gclid=CjwKCAiAt8bIBhBpEiwAzH1w6fnK0SrFgRSVS74gShFIssUkK-NtBy-bFTrwATiAMHd0PT4Wc42CxxoCf2IQAvD_BwEhat/198077201458.html",
+            },
+            {
+                title: "Casquette Patagonia",
+                description: "Pour un style inégalé",
+                image: "/img/gift/casquette.webp",
+                link: "https://eu.patagonia.com/fr/fr/product/p-6-label-maclure-five-panel-hat/198077201458.html",
             },
         ],
     },
@@ -103,19 +126,19 @@ const categories = reactive([
             {
                 title: "Apple Watch Hermès - En Mer Orange 49 mm",
                 description: "La classe à la française.",
-                image: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/MGN94?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=QVptOGY1MkJ6VVpIMDZDSHA1c2JVUWtuVHYzMERCZURia3c5SzJFOTlPZ1JyNmpKMEI2VStpRkVmclovWERhZldlRWJUR3Y4M1l2UE5FN0JZYm1FQ0E",
+                image: "/img/gift/bracelet-hermes.jpeg",
                 link: "https://www.apple.com/fr/shop/product/mgn94zm/a/apple-watch-hermès-en-mer-orange-49-mm?fnode=a81e245e20e8c0f0de080ac2a71d6ad6fa09e0ee1f8db0500258b76b84dab0a1233eb5aece62ac9b42240b23d0b82589991992eb53ac45a84aa36c1379a6e9b3a370295159eb6a753268f7d217a9a60c",
             },
             {
                 title: "AirTag",
                 description: "Pour traquer le vélo",
-                image: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/airtag-single-select-202104?wid=940&hei=1112&fmt=png-alpha&.v=a2ZqcUtUS1VMaUZQNkl6T3JzY1ZmM2VtMnRWRDBsa0dSNys0czlzRGpsWUcvVUdZSjVwcUlDeFRXWEVzclZJSHg2bitObzZwQzk4cEorV1dZdzhIa3pOYWxjbnhLdlBxcWZOdnQwOWRQZk0",
+                image: "/img/gift/airtag.png",
                 link: "https://amzn.eu/d/1uFeWES",
             },
-						{
+            {
                 title: "Visio Pro M5",
                 description: "Incroyablement incroyable",
-                image: "https://www.apple.com/v/apple-vision-pro/i/images/specs/hero__cvgr5aj1ttsi_small_2x.jpgairtag-single-select-202104?wid=940&hei=1112&fmt=png-alpha&.v=a2ZqcUtUS1VMaUZQNkl6T3JzY1ZmM2VtMnRWRDBsa0dSNys0czlzRGpsWUcvVUdZSjVwcUlDeFRXWEVzclZJSHg2bitObzZwQzk4cEorV1dZdzhIa3pOYWxjbnhLdlBxcWZOdnQwOWRQZk0",
+                image: "/img/gift/vision-pro.jpg",
                 link: "https://www.apple.com/fr/shop/buy-vision/apple-vision-pro",
             },
         ],
@@ -126,29 +149,11 @@ const categories = reactive([
             {
                 title: "Support pour AirTag",
                 description: "Pour cacher le AirTag sur le vélo.",
-                image: "https://m.media-amazon.com/images/I/41qmS5Nh93L._AC_.jpg",
+                image: "/img/gift/supp-airtag.jpg",
                 link: "https://amzn.eu/d/8eoF0ew",
             },
-						{
-								title: "Casquette Patagonia",
-                description: "Pour un style inégalé",
-                image: "https://eu.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw248ba34d/images/hi-res/22321_ELGR.jpg?sw=1920&sh=1920&sfrm=png&q=90&bgcolor=f3f4ef",
-                link: "https://eu.patagonia.com/fr/fr/product/p-6-label-maclure-five-panel-hat/198077201458.html",
-						},
-						{
-								title: "Lego La tête dans les étoiles avec Pharrell Williams",
-                description: "C'est magnifique non ?",
-                image: "https://static.fnac-static.com/multimedia/Images/FR/MDM/48/00/a3/27459656/1520-1/tsp20250225135647/LEGO-Icons-10391-La-tete-dans-les-etoiles-avec-Pharrell-Williams-Exclusivite-Fnac.jpg/-/Sites-patagonia-master/default/dw248ba34d/images/hi-res/22321_ELGR.jpg?sw=1920&sh=1920&sfrm=png&q=90&bgcolor=f3f4ef",
-                link: "https://www.fnac.com/LEGO-Icons-10391-La-tete-dans-les-etoiles-avec-Pharrell-Williams-Exclusivite-Fnac/a20974622/w-4?storecode=6902&Origin=SEA_GOOGLE_PLA_JOUET&esl-k=sem-google%7Cnx%7Cc%7Cm%7Ck%7Cp%7Ct%7Cdm%7Ca20461236398%7Cg20461236398&gclsrc=aw.ds&gad_source=1&gad_campaignid=20455746609&gclid=CjwKCAiAt8bIBhBpEiwAzH1w6fnK0SrFgRSVS74gShFIssUkK-NtBy-bFTrwATiAMHd0PT4Wc42CxxoCf2IQAvD_BwEhat/198077201458.html",
-						},
-						{
-								title: "Sweat à capuche",
-                description: "S'habiller comme un développeur",
-                image: "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/471808/sub/goods_471808_sub14_3x4.jpg?width=60027459656/1520-1/tsp20250225135647/LEGO-Icons-10391-La-tete-dans-les-etoiles-avec-Pharrell-Williams-Exclusivite-Fnac.jpg/-/Sites-patagonia-master/default/dw248ba34d/images/hi-res/22321_ELGR.jpg?sw=1920&sh=1920&sfrm=png&q=90&bgcolor=f3f4ef",
-                link: "https://https://www.uniqlo.com/fr/fr/products/E471808-000/00?colorDisplayCode=58&sizeDisplayCode=005storecode=6902&Origin=SEA_GOOGLE_PLA_JOUET&esl-k=sem-google%7Cnx%7Cc%7Cm%7Ck%7Cp%7Ct%7Cdm%7Ca20461236398%7Cg20461236398&gclsrc=aw.ds&gad_source=1&gad_campaignid=20455746609&gclid=CjwKCAiAt8bIBhBpEiwAzH1w6fnK0SrFgRSVS74gShFIssUkK-NtBy-bFTrwATiAMHd0PT4Wc42CxxoCf2IQAvD_BwEhat/198077201458.html",
-						}
-        ]
-    }
+        ],
+    },
 ]);
 
 useHead({
@@ -171,7 +176,7 @@ useHead({
         {
             name: "og:image",
             content: "https://cadeaux.smnb.fr/og-image.png",
-        }
+        },
     ],
     link: [
         {
